4bd979b09d820c5549c9818656d484513166aa58
lingarao yechuri <yechurilingarao25@gmail.com>
2021-01-30 15:40:24 -0500

    mongo DB connection changes

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/MyBlog', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var path = require('path');
 
+const blogschema =  new mongoose.Schema ({
+  title: String,
+  message: String
+});
 
-//const date1 = require( __dirname + "/date.js");
+const blog = mongoose.model('blog', blogschema);
 
 var titleList = [];
 var messageList = [];
@@ -20,16 +26,24 @@ var messageList = [];
 // };
 //list.push(list);
 app.get('/Home', (req, res) => {
-  //  let day = date1();
+
   console.log("testing");
-  //console.log(blogList);
-  //console.log("it is list"+ list[0]);
-  var trimmedmessageList = [];
-  for(var i = 0; i<titleList.length; i++){
-    var trimmedmessage =  messageList[i].substring(0,100);
-    trimmedmessageList.push(trimmedmessage);
-  }
-  res.render("home", {title:titleList, message:trimmedmessageList});
+  // var trimmedmessageList = [];
+  // for(var i = 0; i<titleList.length; i++){
+  //   var trimmedmessage =  messageList[i].substring(0,100);
+  //   trimmedmessageList.push(trimmedmessage);
+  // }
+  // res.render("home", {title:titleList, message:trimmedmessageList});
+  blog.find(function(err, foundBlogs){
+      if(err){
+        console.log(err);
+      }else {
+        console.log(foundBlogs);
+        res.render("home", {blogs:foundBlogs});
+      }
+  })
+
+
 
 });
 
@@ -60,19 +74,39 @@ app.get('/:post', (req, res) => {
   var currentPath = req.params.post;
   var currentPath1 = _.lowerCase(currentPath);
   console.log("currentPath1"+currentPath1);
-  console.log(currentPath1);
-  for(var i = 0; i<titleList.length; i++){
-       console.log(i);
-       console.log(titleList[i]);
-       var currentTitle = _.lowerCase(titleList[i]);
-       console.log("currentTitle"+ currentTitle);
-      if(currentTitle === currentPath1){
-        res.render("posts", {title:titleList[i], message:messageList[i]});
-        console.log("Match Found!");
+  //console.log(currentPath1);
+  blog.find(function(err, foundBlogs){
+      if(err){
+        console.log(err);
       }else {
-        console.log("Match Not Found!");
+          //console.log(foundBlogs);
+          for (var i = 0; i < foundBlogs.length; i++) {
+
+            console.log("foundBlogs.title"+ foundBlogs[i].title);
+            var currentTitle = _.lowerCase(foundBlogs[i].title);
+            console.log("currentTitle"+ currentTitle);
+             if(currentTitle === currentPath1){
+               res.render("posts", {title:foundBlogs[i].title, message:foundBlogs[i].message});
+               console.log("Match Found!");
+             }else {
+               console.log("Match Not Found!");
+             }
+          }
       }
-  }
+  })
+
+  // for(var i = 0; i<titleList.length; i++){
+  //      console.log(i);
+  //      console.log(titleList[i]);
+  //      var currentTitle = _.lowerCase(titleList[i]);
+  //      console.log("currentTitle"+ currentTitle);
+  //     if(currentTitle === currentPath1){
+  //       res.render("posts", {title:titleList[i], message:messageList[i]});
+  //       console.log("Match Found!");
+  //     }else {
+  //       console.log("Match Not Found!");
+  //     }
+  // }
   //res.send(req.params.post);
 //
 
@@ -83,6 +117,8 @@ app.post('/', (req, res) => {
    console.log(req.body);
    //blogList.title = req.body.title;
    //blogList.message = req.body.Message;
+   const blog1 = new blog({title: req.body.title, message:req.body.Message});
+   blog1.save();
    titleList.push(req.body.title);
    messageList.push(req.body.Message);
    console.log(titleList);
